5c104a3621c95439d27cc8ade4ba444a5798aa21
limonadev <limonadev@whiteleaf.dev>
2020-10-07 17:28:29 -0500

    Added BASIC button_style_schema

---
+++ lib/src/schema/schemas/objects/button_style_schema.dart
@@ -0,0 +1,37 @@
+import 'package:json_theme/json_theme_schemas.dart';
+
+class ButtonStyleSchema {
+  static const id =
+      'https://peifferinnovations.com/json_theme/schemas/button_style';
+
+  static final schema = {
+    r'$schema': 'http://json-schema.org/draft-06/schema#',
+    r'id': '$id',
+    'title': 'ButtonStyle',
+    'additionalProperties': false,
+    'properties': {
+      'animationDuration': SchemaHelper.numberSchema,
+      'backgroundColor': SchemaHelper.objectSchema(ColorSchema.id),
+      'elevation': SchemaHelper.numberSchema,
+      'enableFeedback': SchemaHelper.boolSchema,
+      'foregroundColor': SchemaHelper.objectSchema(ColorSchema.id),
+      // TODO: Add SizeSchema
+      /*'minimumSize': {
+        'type': 'array',
+        'items': SchemaHelper.numberSchema,
+      },*/
+      'mouseCursor': SchemaHelper.objectSchema(MouseCursorSchema.id),
+      'overlayColor': SchemaHelper.objectSchema(ColorSchema.id),
+      'padding': SchemaHelper.objectSchema(EdgeInsetsGeometrySchema.id),
+      'shadowColor': SchemaHelper.objectSchema(ColorSchema.id),
+      // TODO: Add OutlinedBorderSchema
+      //'shape': SchemaHelper.objectSchema(),
+      'side': SchemaHelper.objectSchema(BorderSideSchema.id),
+      'materialTapTargetSize': SchemaHelper.objectSchema(
+        MaterialTapTargetSizeSchema.id,
+      ),
+      'textStyle': SchemaHelper.objectSchema(TextStyleSchema.id),
+      'visualDensity': SchemaHelper.objectSchema(VisualDensitySchema.id),
+    },
+  };
+}

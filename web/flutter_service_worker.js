705991571790986b8b024a015e8262b3f3a12f50
root <noop@github.com>
2022-10-11 23:23:02 +0000

    Pages 58

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "7f21bb1cca136083839c302a2438b986",
-"index.html": "c4e613958667ac43a77827d1264647c8",
-"/": "c4e613958667ac43a77827d1264647c8",
+"index.html": "d61ba9fefef3538e73eb7f85c0081a19",
+"/": "d61ba9fefef3538e73eb7f85c0081a19",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
@@ -24,7 +24,7 @@ const RESOURCES = {
 "assets/assets/themes/default.json": "99914b932bd37a50b983c5e7c90ae93b",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/NOTICES": "13012e2768af008d7f1d5d86503ba5c7",
-"version.json": "d76b7928a3a655096e654022444a3c4e",
+"version.json": "696a3ac9b766733ba01fcd4f1c797b6e",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1"
 };
